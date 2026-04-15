43816af5b59e6c421506552496be72d9f049556a
Zahidaziz2005 <zahidaziz2005@gmail.com>
2026-04-15 21:58:14 +0500

    updation footer

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,14 +1,43 @@
 <?php
-echo '    <div class="card text-center">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-  <div class="card-footer text-body-secondary">
-    copy right 2026
-  </div>';
+echo '
+<footer class="bg-dark text-white mt-5">
+    <div class="container py-4">
+
+        <div class="row">
+
+            <!-- 🔷 About -->
+            <div class="col-md-4">
+                <h5>iDiscuss</h5>
+                <p>A platform to discuss coding problems, share knowledge, and grow together.</p>
+            </div>
+
+            <!-- 🔷 Quick Links -->
+            <div class="col-md-4">
+                <h5>Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="index.php" class="text-white">Home</a></li>
+                    <li><a href="about.php" class="text-white">About</a></li>
+                    <li><a href="contact.php" class="text-white">Contact</a></li>
+                </ul>
+            </div>
+
+            <!-- 🔷 Contact Info -->
+            <div class="col-md-4">
+                <h5>Contact</h5>
+                <p>Email: support@idiscuss.com</p>
+                <p>Location: Pakistan</p>
+            </div>
+
+        </div>
+
+        <hr style="background-color:white;">
+
+        <!-- 🔷 Bottom -->
+        <div class="text-center">
+            <p class="mb-0">© 2026 iDiscuss | Developed by Zahid Aziz</p>
+        </div>
+
+    </div>
+</footer>
+';
 ?>
